920462a9b57ee5e7c46c76244780237289a6e389
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-02-13 17:20:58 -0600

    added title to header, re-styled

--- src/Header/index.js
+++ src/Header/index.js
@@ -5,9 +5,14 @@ const Header = (props) => {
 
 	return (
 		<header>
-			<div>
-				<h1> Magic: The Gathering </h1>
-				<h3> Deckbuilder App </h3>
+			<div className="headerCon">
+				<div className="titleCon">
+					<h1> CardPool </h1>
+				</div>
+				<div className="subtitleCon">
+					<h3> Magic: The Gathering </h3>
+					<h3> Deckbuilder App </h3>
+				</div>
 			</div>
 			<nav>
 				{ props.authData.loggedIn ? <p><small>Logged in as {props.authData.loggedInAs}</small></p> : null }
